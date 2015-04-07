2d68b7818e3169cb4f704751f248657749818027
Freakness109 <freakness109@gmail.com>
2015-04-07 13:49:22 +0200

    Setup a bunch of base classes

---
+++ src/main/java/ch/freakness109/EnderMod/item/AxeEnderMod.java
@@ -0,0 +1,39 @@
+package ch.freakness109.EnderMod.item;
+
+import ch.freakness109.EnderMod.creativetab.CreativeTabEnderMod;
+import ch.freakness109.EnderMod.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by Axel on 07.04.2015.
+ */
+public class AxeEnderMod extends ItemAxe {
+    public AxeEnderMod(ToolMaterial material) {
+        super(material);
+        this.setCreativeTab(CreativeTabEnderMod.ENDERMOD_TAB);
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack) {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister) {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName) {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
